e9ecc14b964323b7fef4ad5ace69d22a3a472afd
Chris Hager <chris@linuxuser.at>
2022-09-20 20:18:47 +0200

    added httplogger.LoggingMiddlewareZap (#16)

--- examples/httplogger/main.go
+++ examples/httplogger/main.go
@@ -4,15 +4,15 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/flashbots/go-utils/httplogger"
+	"go.uber.org/zap"
 )
 
 var (
 	listenAddr = "localhost:8124"
-	logJSON    = os.Getenv("LOG_JSON") == "1"
+	// logJSON    = os.Getenv("LOG_JSON") == "1"
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,20 +29,27 @@ func PanicHandler(w http.ResponseWriter, r *http.Request) {
 	panic("foo!")
 }
 
-func main() {
-	logFormat := log.TerminalFormat(true)
-	if logJSON {
-		logFormat = log.JSONFormat()
-	}
+func getZapLogger() *zap.SugaredLogger {
+	// logger, _ := zap.NewDevelopment()
+	logger, _ := zap.NewProduction()
+	log := logger.Sugar()
+	return log
+}
 
-	log.Root().SetHandler(log.LvlFilterHandler(log.LvlDebug, log.StreamHandler(os.Stderr, logFormat)))
+func main() {
+	// logFormat := log.TerminalFormat(true)
+	// if logJSON {
+	// 	logFormat = log.JSONFormat()
+	// }
+
+	// log.Root().SetHandler(log.LvlFilterHandler(log.LvlDebug, log.StreamHandler(os.Stderr, logFormat)))
 
 	fmt.Println("Webserver running at", listenAddr)
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", HelloHandler)
 	mux.HandleFunc("/error", ErrorHandler)
 	mux.HandleFunc("/panic", PanicHandler)
-	loggedRouter := httplogger.LoggingMiddleware(mux)
+	loggedRouter := httplogger.LoggingMiddlewareZap(getZapLogger(), mux)
 	if err := http.ListenAndServe(listenAddr, loggedRouter); err != nil {
 		log.Crit("webserver failed", "err", err)
 	}
--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/sirupsen/logrus v1.9.0
 	github.com/stretchr/testify v1.8.0
 	go.uber.org/atomic v1.10.0
+	go.uber.org/zap v1.23.0
 )
 
 require (
@@ -23,6 +24,7 @@ require (
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
 	golang.org/x/crypto v0.0.0-20220919173607-35f4265a4bc0 // indirect
 	golang.org/x/sys v0.0.0-20220919091848-fb04ddd9f9c8 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
