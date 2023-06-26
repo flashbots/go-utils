ba749b033bf054a45543608a8114010da347eb86
Anton Bronnikov <anton@northernforest.nl>
2023-06-26 10:48:33 +0200

    [feat] Pass zap logger with context

--- examples/httplogger/main.go
+++ examples/httplogger/main.go
@@ -2,17 +2,17 @@ package main
 
 import (
 	"errors"
-	"fmt"
+	"flag"
 	"net/http"
 
 	"github.com/ethereum/go-ethereum/log"
 	"github.com/flashbots/go-utils/httplogger"
+	"github.com/flashbots/go-utils/logutils"
 	"go.uber.org/zap"
 )
 
 var (
 	listenAddr = "localhost:8124"
-	// logJSON    = os.Getenv("LOG_JSON") == "1"
 )
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
@@ -21,7 +21,8 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func ErrorHandler(w http.ResponseWriter, r *http.Request) {
-	log.Error("this is an error", "err", errors.New("testError"))
+	l := logutils.ZapFromRequest(r)
+	l.Error("this is an error", zap.Error(errors.New("testError")))
 	http.Error(w, "this is an error", http.StatusInternalServerError)
 }
 
@@ -29,29 +30,26 @@ func PanicHandler(w http.ResponseWriter, r *http.Request) {
 	panic("foo!")
 }
 
-func getZapLogger() *zap.SugaredLogger {
-	// logger, _ := zap.NewDevelopment()
-	logger, _ := zap.NewProduction()
-	log := logger.Sugar()
-	return log
-}
-
 func main() {
-	// logFormat := log.TerminalFormat(true)
-	// if logJSON {
-	// 	logFormat = log.JSONFormat()
-	// }
+	logLevel := flag.String("log-level", "info", "Log level")
+	logDev := flag.Bool("log-dev", false, "Log in development mode")
+	flag.Parse()
 
-	// log.Root().SetHandler(log.LvlFilterHandler(log.LvlDebug, log.StreamHandler(os.Stderr, logFormat)))
+	l := logutils.GetZapLogger(
+		logutils.LogDevMode(*logDev),
+		logutils.LogLevel(*logLevel),
+	)
+	defer logutils.FlushZap(l)
 
-	fmt.Println("Webserver running at", listenAddr)
+	l.Info("Webserver running at " + listenAddr)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/hello", HelloHandler)
 	mux.HandleFunc("/error", ErrorHandler)
 	mux.HandleFunc("/panic", PanicHandler)
-	loggedRouter := httplogger.LoggingMiddlewareZap(getZapLogger(), mux)
+	loggedRouter := httplogger.LoggingMiddlewareZap(l, mux)
+
 	if err := http.ListenAndServe(listenAddr, loggedRouter); err != nil {
 		log.Crit("webserver failed", "err", err)
 	}
-
 }
