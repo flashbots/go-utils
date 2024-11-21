8ec0c431c5d9c5c72fa76edaae02933d9878d45e
Chris Hager <chris@linuxuser.at>
2024-11-21 19:00:10 +0100

    rpcserver: GET / support (#36)

---
+++ examples/rpcserver/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/flashbots/go-utils/rpcserver"
+)
+
+var listenAddr = ":8080"
+
+func main() {
+	handler, err := rpcserver.NewJSONRPCHandler(
+		rpcserver.Methods{
+			"test_foo": HandleTestFoo,
+		},
+		rpcserver.JSONRPCHandlerOpts{
+			ServerName:         "public_server",
+			GetResponseContent: []byte("Hello world"),
+		},
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	// server
+	server := &http.Server{
+		Addr:    listenAddr,
+		Handler: handler,
+	}
+	fmt.Println("Starting server.", "listenAddr:", listenAddr)
+	if err := server.ListenAndServe(); err != nil {
+		panic(err)
+	}
+}
+
+func HandleTestFoo(ctx context.Context) (string, error) {
+	return "foo", nil
+}
